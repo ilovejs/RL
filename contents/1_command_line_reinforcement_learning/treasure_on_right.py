3773f3fc9b7f4c04d5b3ab81c5af1e466cf1cec1
Michael <zhuangdeyouxiang@gmail.com>
2018-06-03 15:47:24 +1000

    intellij Q-learning

--- contents/1_command_line_reinforcement_learning/treasure_on_right.py
+++ contents/1_command_line_reinforcement_learning/treasure_on_right.py
@@ -19,7 +19,7 @@ EPSILON = 0.9   # greedy police
 ALPHA = 0.1     # learning rate
 GAMMA = 0.9    # discount factor
 MAX_EPISODES = 13   # maximum episodes
-FRESH_TIME = 0.3    # fresh time for one move
+FRESH_TIME = 0.01    # fresh time for one move e.g. 0.01 is quicker
 
 
 def build_q_table(n_states, actions):
@@ -53,7 +53,7 @@ def get_env_feedback(S, A):
     else:   # move left
         R = 0
         if S == 0:
-            S_ = S  # reach the wall
+            S_ = 0  # reach the wall
         else:
             S_ = S - 1
     return S_, R
@@ -77,27 +77,35 @@ def update_env(S, episode, step_counter):
 def rl():
     # main part of RL loop
     q_table = build_q_table(N_STATES, ACTIONS)
+
     for episode in range(MAX_EPISODES):
+
         step_counter = 0
         S = 0
         is_terminated = False
+
         update_env(S, episode, step_counter)
+
         while not is_terminated:
 
             A = choose_action(S, q_table)
-            S_, R = get_env_feedback(S, A)  # take action & get next state and reward
+            S_, R = get_env_feedback(S, A)                         # take action & get next state and reward
             q_predict = q_table.loc[S, A]
+
             if S_ != 'terminal':
+                # TOOD: iloc is a mistery
                 q_target = R + GAMMA * q_table.iloc[S_, :].max()   # next state is not terminal
             else:
-                q_target = R     # next state is terminal
+                q_target = R            # next state is terminal
                 is_terminated = True    # terminate this episode
 
-            q_table.loc[S, A] += ALPHA * (q_target - q_predict)  # update
-            S = S_  # move to next state
+            q_table.loc[S, A] += ALPHA * (q_target - q_predict)    # update
+            S = S_                                                 # move to next state
 
             update_env(S, episode, step_counter+1)
+
             step_counter += 1
+
     return q_table
 
 
